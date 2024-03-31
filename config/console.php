46344e07878b79ba792b4e73b8a1d1f99f31b991
Andrii Shynkarenko <91028628+shynkarenko-andrii@users.noreply.github.com>
2024-04-01 00:49:19 +0300

    Uncomment app/migrations (#3)
    App/migrations section uncommented

--- config/console.php
+++ config/console.php
@@ -56,7 +56,7 @@ $config = [
         'migrate' => [
             'class' => \yii\console\controllers\MigrateController::class,
             'migrationPath' => [
-//                '@app/migrations',
+                '@app/migrations',
                 '@yii/rbac/migrations', // Just in case you forgot to run it on console (see next note)
             ],
             'migrationNamespaces' => [
